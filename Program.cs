fc625d0708a0b5b7b01c59d86e6680d21bfa5ace
Faithe Olson <faithe.olson@gmail.com>
2018-01-24 15:15:52 -0500

    With For Loop

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer between 1 and 10:");
+            int Input = int.Parse(Console.ReadLine());
+            int Solution = Input;
+
+            for (int i = 1; i < Input; i++)
+            {
+                Solution *= (Input-i);
+            }
+
+            Console.WriteLine(Solution);
+            
+        }
+    }
+}
